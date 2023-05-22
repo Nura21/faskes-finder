e5d981b075e592a51a2ef46218f33448331b847b
anggawijaya7122 <anggawijaya7122@gmail.com>
2023-05-23 01:07:23 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,41 +1,11 @@
 @extends('layouts.navigations.default')
-@section('title', 'Penjualan Dashboard')
+@section('title', 'Dashboard')
 @section('body-class', 'hold-transition sidebar-mini layout-fixed')
 @section('main-menu', 'Dashboard')
 @section('menu', 'Dashboard')
 @section('process', 'List')
 @section('main')
     <div class="row">
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-                <div class="inner">
-                    {{-- <h3>{{ $transactions }}</h3> --}}
-
-                    <p>Transactions</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    {{-- <h3>{{ $suppliers }}<sup style="font-size: 20px"></sup></h3> --}}
-
-                    <p>Suppliers</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-person"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
         <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-warning">
@@ -50,50 +20,5 @@
                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-                <div class="inner">
-                    {{-- <h3>{{ $stuffs }}</h3> --}}
-
-                    <p>Stuffs</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    {{-- <h3>{{ $clients }}</h3> --}}
-
-                    <p>Clients</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-person"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-                <div class="inner">
-                    {{-- <h3>{{ $payments }}</h3> --}}
-
-                    <p>Payments</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
     </div>
 @endsection
